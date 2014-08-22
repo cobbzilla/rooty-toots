a078c2e53fd8a9710f51148bbc8f8185efce3512
jonathan <jonathan@kyuss.org>
2014-08-21 21:06:37 -0700

    use chained accessors to reduce boilerplate code

--- src/main/java/rooty/toots/djbdns/DnsMessage.java
+++ src/main/java/rooty/toots/djbdns/DnsMessage.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 import rooty.RootyMessage;
 
+@Accessors(chain=true)
 @NoArgsConstructor @AllArgsConstructor
 public class DnsMessage extends RootyMessage {
 
     @Getter @Setter private String line;
-    public DnsMessage withLine (String s) { line = s; return this; }
 
 }
