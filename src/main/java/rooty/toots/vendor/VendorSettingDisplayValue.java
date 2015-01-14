b472dbcdeb765fbab1b34ace596aad3169895fab
jonathan <jonathan@kyuss.org>
2015-01-13 18:38:52 -0800

    add readOnly flag

--- src/main/java/rooty/toots/vendor/VendorSettingDisplayValue.java
+++ src/main/java/rooty/toots/vendor/VendorSettingDisplayValue.java
@@ -8,7 +8,13 @@ import lombok.Setter;
 @NoArgsConstructor @AllArgsConstructor
 public class VendorSettingDisplayValue {
 
+    public VendorSettingDisplayValue (String path, String value) {
+        this.path = path;
+        this.value = value;
+    }
+
     @Getter @Setter private String path;
     @Getter @Setter private String value;
+    @Getter @Setter private boolean readOnly = false;
 
 }
