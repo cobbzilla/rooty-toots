64ab2dfefea5a43e93dd0bc2bf5915887563520c
jonathan <jonathan@kyuss.org>
2015-08-24 06:54:41 -0700

    migrate to commons-lang3

--- src/main/java/rooty/toots/djbdns/DnsLineParser.java
+++ src/main/java/rooty/toots/djbdns/DnsLineParser.java
@@ -1,7 +1,7 @@
 package rooty.toots.djbdns;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.cobbzilla.util.dns.DnsRecord;
 import org.cobbzilla.util.dns.DnsType;
 
